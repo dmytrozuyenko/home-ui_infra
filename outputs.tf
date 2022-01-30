0e7e5de5a692b5e282d5804b911ca69437d24d23
dmytrozuyenko <95592566+dmytrozuyenko@users.noreply.github.com>
2022-01-31 00:10:25 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-output "public_ip" {
-  description = "The public IP address"
-  value       = try(aws_instance.this[0].public_ip, aws_spot_instance_request.this[0].public_ip, "")
+output "instance_ips" {
+  value = aws_instance.web.*.public_ip
 }
