af4062ba311e6edc192ad9b1cca2fa885b2bd88b
dmytrozuyenko <95592566+dmytrozuyenko@users.noreply.github.com>
2022-02-03 00:15:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 variable "access_key"{}
 variable "secret_key"{}
-variable "aws_public_key"{}
 
 
 provider "aws" {
@@ -8,7 +7,6 @@ provider "aws" {
  access_key = var.access_key
  secret_key = var.secret_key
 }
-
 
 
 data "aws_ami" "ubuntu" {
@@ -40,38 +38,47 @@ resource "aws_instance" "home-ui" {
  tags = {
    Name = "home-ui"
  }
-#  provisioner "local-exec" {
-#    command = "sudo echo '${var.aws_public_key}' > /home/ubuntu/.ssh/authorized_keys"
-#  }
-}
-
-# resource "aws_key_pair" "home" {
-#   public_key = ${var.aws_public_key}
-# }
-
-# module "key_pair_external" {
-#   source = "../../"
-
-#   key_name   = "home"
-#   public_key = "${var.aws_public_key}"
-
-#   tags = {
-#     External = "yes"
-#   }
-# }
 
 resource "aws_security_group" "home-ui_sg" {
  name        = "home-ui_sg"
  description = "Traffic rules for Home-UI"
 
- ingress {
-   from_port        = 0
-   to_port          = 0
+  ingress {
+   description      = "HTTP"
+   from_port        = 80
+   to_port          = 80
    protocol         = "-1"
    cidr_blocks      = ["0.0.0.0/0"]
    ipv6_cidr_blocks = ["::/0"]
  }
+ 
+ ingress {
+   description      = "HTTPS"
+   from_port        = 443
+   to_port          = 443
+   protocol         = "-1"
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+ }
+ 
+ ingress {
+   description      = "Jenkins"
+   from_port        = 22
+   to_port          = 22
+   protocol         = "-1"
+   cidr_blocks      = ["34.123.118.107/32"]
+   ipv6_cidr_blocks = ["::/0"]
+ }
 
+  ingress {
+   description      = "Desktop"
+   from_port        = 22
+   to_port          = 22
+   protocol         = "-1"
+   cidr_blocks      = ["46.119.254.233/32"]
+   ipv6_cidr_blocks = ["::/0"]
+ }
+ 
  egress {
    from_port        = 0
    to_port          = 0
@@ -83,17 +90,4 @@ resource "aws_security_group" "home-ui_sg" {
  tags = {
    Name = "home-ui"
  }
-#   provisioner "remote-exec" {
-#    inline = [
-#      "sudo echo '${var.aws_public_key}' > /home/ubuntu/.ssh/authorized_keys",
-#      "sudo mv authorized_keys /home/myuser/.ssh",
-#      "sudo chmod 700 /home/ubuntu/.ssh",
-#      "sudo chmod 600 /home/ubuntu/.ssh/authorized_keys",
-#      "sudo usermod -aG sudo ubuntu"
-#    ]
-#  }
-#  connection {
-#    user = "ubuntu"
-#    host = "home-ui"
-
 }
