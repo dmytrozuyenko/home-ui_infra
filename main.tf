93777af2c92c859db9cb26f8e4468adda67b324a
dmytrozuyenko <95592566+dmytrozuyenko@users.noreply.github.com>
2022-02-03 11:59:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_security_group" "home-ui_sg" {
  }
  
  ingress {
-   description      = "Jenkins"
+   description      = "Jenkins and desktop"
    from_port        = 22
    to_port          = 22
    protocol         = "tcp"
